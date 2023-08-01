e521786db409ce863f3cd4fa33eeefdb6eede8a0
George60tny <129588195+George60tny@users.noreply.github.com>
2023-08-01 11:22:19 +0300

    Commit 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("George", "Shestidesiatni");
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
+}
+
+class Person {
+    private String name;
+    private String lastName;
+
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
