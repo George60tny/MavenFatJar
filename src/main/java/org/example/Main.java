35b856ccfb8270efafe427dfbf83f4447f934ad5
George60tny <129588195+George60tny@users.noreply.github.com>
2023-08-01 13:29:56 +0300

    Commit 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,12 +11,3 @@ public class Main {
     }
 }
 
-class Person {
-    private String name;
-    private String lastName;
-
-    public Person(String name, String lastName) {
-        this.name = name;
-        this.lastName = lastName;
-    }
-}
